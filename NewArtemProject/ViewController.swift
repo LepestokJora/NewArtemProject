2b46ee9f7f0ad953f8ae024eec8ba52d26172b88
Artem Peshkov <pewkoff@gmail.com>
2024-09-13 22:30:16 +0300

    home work four in modul two (full name)

--- NewArtemProject/ViewController.swift
+++ NewArtemProject/ViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     let nameUser = "Артем"
-
+    let surNameUser = "Пешков"
+    
+    var fullNameUser: String {
+        return ("\(nameUser) \(surNameUser)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Меня зовут \(nameUser), я создал свое первое приложение")
+        print("Меня зовут \(fullNameUser), я создал свое первое приложение")
         
     }
-
-
+    
+    
+    
 }
 
