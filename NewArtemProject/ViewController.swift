8193f6abf518085e289d87795564227304e3510b
Artem Peshkov <pewkoff@gmail.com>
2024-09-13 21:52:49 +0300

    home Work threen in modul two

---
+++ NewArtemProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NewArtemProject
+//
+//  Created by Lepestok Jora on 13.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let nameUser = "Артем"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Меня зовут \(nameUser), я создал свое первое приложение")
+        
+    }
+
+
+}
+
